9384b747a68ab5a001c1c5a57e3201a4512a26c0
RaulRuizCadena <Raul_Ruiz_C@yahoo.com>
2024-02-14 17:16:36 -0500

    Agregado el modelo de Grupos

---
+++ Models/Grupos.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace IngGasAPI.Models
+{
+    public class Grupos
+    {
+        [Key]
+        public int gruId { get; set; }
+        [Required]
+        public string gruCodigo { get; set; }
+        [Required(ErrorMessage = "Debe dar el Nombre del Grupo")]
+        public string gruNombre { get; set; }
+        [Required(ErrorMessage = "Debe dar el Tipo del Grupo")]
+        public string gruTipo { get; set; }
+        [Required]
+        [NotMapped]
+        public int usuCreoId { get; set; }
+        [Required]
+        [NotMapped]
+        public DateTime gruFechaCreo { get; set; }
+        [NotMapped]
+        public int? usuActualizoId { get; set; }
+        [NotMapped]
+        public DateTime? gruFechaActualizo { get; set; }
+    }
+}
