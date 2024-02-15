3f2d50fc51b293ee32fdf5c44b5f7aed3e47c215
RaulRuizCadena <Raul_Ruiz_C@yahoo.com>
2024-02-15 04:14:41 -0500

    Add Interface Grupos

---
+++ Interfaces/IGruposService.cs
@@ -0,0 +1,15 @@
+﻿using IngGasAPI.Models;
+
+namespace IngGasAPI.Interfaces
+{
+    public interface IGruposService
+    {
+        public Task<List<Grupos>> ListaGrupos(string cadOrden, int MSSoapWS);
+        public Task<IEnumerable<Grupos>> GetGrupoByIdAsync(int Id);
+        public Task<int> AddGrupo(Grupos grupo);
+        public Task<int> AddGrupo2(int gruId, string gruNombre, string gruTipo, int usuId);
+        public Task<int> UpdateGrupo(int gruId, string gruNombre, string gruTipo, int usuId);
+        public Task<int> EditGrupo(Grupos grupo);
+        public Task<int> DeleteGrupo(int gruId);
+    }
+}
